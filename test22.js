96f9c74bab397d749e670e6905176bca44771cea
kooks7 <busanminjae@naver.com>
2020-05-30 20:53:11 +0900

    Solving Problem

--- test22.js
+++ test22.js
@@ -42,18 +42,18 @@ const stringifyNumbers = obj => {
   return obj;
 };
 
-// function stringifyNumbers(obj) {
-//     var newObj = {};
-//     for (var key in obj) {
-//       if (typeof obj[key] === 'number') {
-//         newObj[key] = obj[key].toString();
-//       } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
-//         newObj[key] = stringifyNumbers(obj[key]);
-//       } else {
-//         newObj[key] = obj[key];
-//       }
-//     }
-//     return newObj;
-//   }
+function stringifyNumbers(obj) {
+    var newObj = {};
+    for (var key in obj) {
+      if (typeof obj[key] === 'number') {
+        newObj[key] = obj[key].toString();
+      } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+        newObj[key] = stringifyNumbers(obj[key]);
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+    return newObj;
+  }
 
 console.log(stringifyNumbers(obj));
